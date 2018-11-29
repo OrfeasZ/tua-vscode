bfe72c1752c118a16bed5af0ff61e994e3820f26
Orfeas Zafeiris <orfeaz@gmail.com>
2018-11-29 03:11:59 +0200

    Add basic analysis and diagnostics to the language server

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,15 @@ import {
     TextDocuments,
     ProposedFeatures,
     InitializeParams,
+    Diagnostic,
+    DiagnosticSeverity,
 } from "vscode-languageserver";
+
+import {
+    Analyzer
+} from "tua/dist/parser/analyzer";
+
+import { TuaError } from 'tua/dist/util/analysisError';
 
 // Create a connection for the server. The connection uses Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
@@ -28,7 +36,27 @@ connection.onInitialized(() => {
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
 documents.onDidChangeContent(change => {
+    const code = change.document.getText();
+    const errors = Analyzer.analyze(code);
 
+    const diagnostics: Diagnostic[] = [];
+
+    for (const error of errors) {
+        const diagnostic: Diagnostic = {
+            severity: DiagnosticSeverity.Error,
+            range: {
+                start: change.document.positionAt(error.indexStart),
+                end: change.document.positionAt(error.indexEnd)
+            },
+            message: TuaError[error.error],
+            code: error.error,
+            source: 'tua'
+        };
+
+        diagnostics.push(diagnostic);
+    }
+
+    connection.sendDiagnostics({ uri: change.document.uri, diagnostics });
 });
 
 // Make the text document manager listen on the connection
